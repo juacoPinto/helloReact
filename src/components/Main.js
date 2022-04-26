1435431f03a0f1e9acda3d06cad3753292302512
joaquin <juacopinto@gmail.com>
2022-04-26 11:26:44 -0400

    First Commit

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Main = () => {
+	return (
+		<>
+			<div className="container bg-grey">
+				<h1>A Warm Welcome!</h1>
+				<p>
+					Lorem Ipsum is simply dummy text of the printing and typesetting
+					industry. Lorem Ipsum has been the industry's standard dummy text ever
+					since the 1500s
+				</p>
+				<button className="btn btn-primary">Call To Action</button>
+			</div>
+		</>
+	);
+};
